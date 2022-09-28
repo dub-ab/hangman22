e474c4235089a2ffe8105de85bb6164c1bef9cb8
dub-ab <anthonywashington@hotmail.com>
2022-09-28 11:22:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# hangman22
+A simple project that utilizes a patterned approach to game design.
+
+This hangman simulation utilizes some of the principals of <a href="https://www.patternsgameprog.com/series/discover-python-and-patterns/">Discover Python and Patterns</a> for project structure.
+
+The <a href="https://www.youtube.com/watch?v=j9yMFG3D7fg&t=16s">Python / Pygame Tutorial: Creating multiple stages in a game</a> video gave the final understanding of how to use a <a href="https://en.wikipedia.org/wiki/Finite-state_machine">finite-state machine </a> (FSM) to create multiple stages.  
---
+++ settings.py
@@ -0,0 +1,18 @@
+""" Settings for hanagman22. """  
+
+import pygame
+
+GAME_SCREEN_WIDTH = 400
+GAME_SCREEN_HEIGHT = 450
+
+# Colors                                    HEX         RGB
+NAVY = pygame.Color('navy')               #000080     (  0,   0, 128, 255)
+DODGERBLUE = pygame.Color('dodgerblue')   #1E90FF     ( 30, 144, 255, 255)
+DARKTEXT = pygame.Color('gray4')          #0A0A0A     ( 10,  10,  10, 255)
+GREEN = pygame.Color('mediumseagreen')    #3CB371     ( 60, 179, 113, 255)
+RED = pygame.Color('red')                 #FF0000     (255,   0,   0, 255)
+LIGHTPINK = pygame.Color('lightpink')     #FFC0CB     (255, 182, 193, 255)
+GRAY = pygame.Color('darkgray')           #808080     (169, 169, 169, 255)
+WHITE = pygame.Color('white')             #FFFFFF     (255, 255, 255, 255)
+DARKGREEN = pygame.Color('darkgreen')     #006400     (  0, 100,   0, 255)
+
